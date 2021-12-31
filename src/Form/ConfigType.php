4a1a74a2306ea18649c9d2ff8df198c8cdbda338
Mark Berntheisel <mark@berntheisel.de>
2021-12-31 15:05:26 +0100

    - redesing forms for config

--- src/Form/ConfigType.php
+++ src/Form/ConfigType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Config;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,23 @@ class ConfigType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dayStartTime')
-            ->add('dayEndTime')
-            ->add('minuteSteps')
+            ->add('dayStartTime', TextType::class, [
+                'label' => 'Tagesbeginn (Bsp.: 8 Uhr => 08:00:00)'
+            ])
+            ->add('dayEndTime', TextType::class, [
+                'label' => 'Tagesende (Bsp.: 20 Uhr => 20:00:00)'
+            ])
+            ->add('minuteSteps', ChoiceType::class, [
+                'label' => 'Minutenintervall für Termine',
+                'choices' => [
+                    '5' => 5,
+                    '10' => 10,
+                    '15' => 15,
+                    '20' => 20,
+                    '25' => 25,
+                    '30' => 30,
+                ],
+            ])
         ;
     }
 
